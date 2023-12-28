8449124da390776ea81d9719d65b45f6eb14cc87
MatiasDAppella <appellamatias@gmail.com>
2023-12-28 12:03:40 -0300

    chore: cors policity added

--- 9.8.-9.9/index.ts
+++ 9.8.-9.9/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
 const app = express();
-app.use(express.json());
 
+// Config
 const PORT = 3000;
 
+// Middlewares
+app.use(express.json());
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+// Enpoints
 app.get("/ping", (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
